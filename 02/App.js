c6f57cab32db7cd20f3cfca9a18dab92112d2929
rishitsrivastava <75729713+rishitsrivastava@users.noreply.github.com>
2024-02-25 22:20:31 +0530

    Delete 02 directory

--- 02/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const heading1= React.createElement("h1",{id:"heading1"}, "from heading1");
-const heading2 = React.createElement("h2",{id:"heading2"}, "from heading2");
-const container = React.createElement("div", {id:"container"},[heading1, heading2])
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container)
