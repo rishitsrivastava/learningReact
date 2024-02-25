b42382175962016edbeb37188ad2c3ce8e812b95
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-25 22:17:33 +0530

    bug

--- 02/App.js
+++ 02/App.js
@@ -6,4 +6,4 @@ const heading2 = React.createElement("h2",{id:"heading2"}, "from heading2");
 const container = React.createElement("div", {id:"container"},[heading1, heading2])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(container)
