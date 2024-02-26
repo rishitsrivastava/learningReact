236781137da4abd7ac9fe921defbc3a6450ffb07
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-26 22:57:04 +0530

    added remove-consolelog

---
+++ 2/dist/index.261be2be.js
@@ -0,0 +1,2 @@
+const e=React.createElement("h1",{id:"heading1"},"from heading1"),t=React.createElement("h2",{id:"heading2"},"from heading2"),a=React.createElement("div",{id:"container"},[e,t]);ReactDOM.createRoot(document.getElementById("root")).render(a);
+//# sourceMappingURL=index.261be2be.js.map
