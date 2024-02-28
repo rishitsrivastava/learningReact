29d4bf51a89061581ec6b30f842551b75c169211
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-28 21:27:36 +0530

    cleaning code

--- 2/index.js
+++ 2/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client"
+import React, { createElement } from "react";
+import ReactDOM, { createRoot } from "react-dom/client"
 
-const heading1= React.createElement("h1",{id:"heading1"}, "from heading1");
-const heading2 = React.createElement("h2",{id:"heading2"}, "from heading2");
-const container = React.createElement("div", {id:"container"},[heading1, heading2])
+const heading1= createElement("h1",{id:"heading1"}, "from heading1");
+const heading2 = createElement("h2",{id:"heading2"}, "from heading2");
+const container = createElement("div", {id:"container"},[heading1, heading2])
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(container);
