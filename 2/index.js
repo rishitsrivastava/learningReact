3d510254ac5cc460c9f025b56d0a6fcfea756eb7
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-28 21:32:32 +0530

    added comments

--- 2/index.js
+++ 2/index.js
@@ -1,8 +1,12 @@
 import React, { createElement } from "react";
 import ReactDOM, { createRoot } from "react-dom/client"
 
+// what does jsx do? 
+// JSX -> createElement -> object -> HTML(DOM)
+
 const heading1= createElement("h1",{id:"heading1"}, "from heading1");
-const heading2 = createElement("h2",{id:"heading2"}, "from heading2");
+// const heading2 = createElement("h2",{id:"heading2"}, "from heading2");
+const heading2 = <h1 id="heading2">from heading2</h1>
 const container = createElement("div", {id:"container"},[heading1, heading2])
 
 const root = createRoot(document.getElementById("root"));
