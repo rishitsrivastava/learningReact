d8c4fad4de334d31ae9c845eb26ed2ac821e8c4b
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-28 22:01:58 +0530

    first functional component

--- 2/index.js
+++ 2/index.js
@@ -7,7 +7,18 @@ import ReactDOM, { createRoot } from "react-dom/client"
 const heading1= createElement("h1",{id:"heading1"}, "from heading1");
 // const heading2 = createElement("h2",{id:"heading2"}, "from heading2");
 const heading2 = <h1 id="heading2">from heading2</h1>
+// both heading1 and heading2 are same, heading2 gets created into heading1 behind the scenes by babel
 const container = createElement("div", {id:"container"},[heading1, heading2])
 
+
+const HeadingComponent = () => {
+    return <>
+        <h1>hey bro from first functional component</h1>
+        <h2>writing two tags in the return statement from functional component</h2>
+    </>
+}
+
+
 const root = createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeadingComponent />);
+
