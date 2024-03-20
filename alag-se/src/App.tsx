8c780df2b814e8465f772070fb92dd7f6b7251b9
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-03-21 01:09:52 +0530

    useRef

--- alag-se/src/App.tsx
+++ alag-se/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [val, setVal] = useState("");
+  const refe = useRef("");
+
+  const handleButton = () => {
+    console.log(val)
+    console.log(refe.current)
+    refe.current.focus();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>working with useref</h1>
+      <label>enter the value you dumb fuck:  </label>
+      <input ref={refe} type="text" placeholder='input 1' value={val} onChange={(e) => {setVal(e.target.value)}} />
+      <button onClick={handleButton}>on Click</button>
+    </div>
   )
 }
 
